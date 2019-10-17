e42bbfbbe1578392d7fdb5a5622092689aae3e1c
ArchieMC <56671040+ArchieMC@users.noreply.github.com>
2019-10-17 11:40:18 +0300

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -34,10 +34,37 @@ xui.Class('App', 'xui.Module',{
                         "target":"url",
                         "args":[ ],
                         "method":"selectFile",
-                        "onOK":0,
-                        "event":1
+                        "event":1,
+                        "onOK":0
                     }
                 ])
+            );
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"xui_ui_button9")
+                .setDirtyMark(false)
+                .setLeft("30.833333333333332em")
+                .setTop("18.333333333333332em")
+                .setHeight("3.8333333333333335em")
+                .setCaption("Image Button")
+                .setImageClass("xui-icon-xui")
+                .onClickDrop([
+                    {
+                        "desc":"Action 1",
+                        "type":"control",
+                        "target":"xui_ui_button9",
+                        "args":[ ],
+                        "method":"fireClickEvent",
+                        "event":1
+                    },
+                    "_xui_ui_button9_onclickdrop"
+                ])
+                .setCustomStyle({
+                    "KEY":{
+                        "color":"#FFE4C4"
+                    }
+                })
             );
             
             return children;
@@ -48,7 +75,19 @@ xui.Class('App', 'xui.Module',{
         customAppend : function(parent, subId, left, top){
             // "return false" will cause all the internal UI controls will be added to the parent panel
             return false;
-        }
+        },
+        /**
+         * Fired when user click the drop button
+         * @method onClickDrop [xui.UI.Button event]
+         * @param {xui.UIProfile.} profile  The current control's profile object
+         * @param {Event} e , Dom event object
+         * @param {Element.xui} src  id or Dom Element
+         * @param {} value  Object
+        */
+        _xui_ui_button9_onclickdrop:function(profile, e, src, value){
+            var ns = this, uictrl = profile.boxing();
+        },
+        functions:{ }
         /*,
         // To determine how properties affects this module
         propSetAction : function(prop){
